1bd34cec90adabac08f6aeb007723a0f654ce53e
drkleena13 <echohack2016@gmail.com>
2016-04-23 01:46:25 +0800

    https://github.com/drkleena/nasaspacemonkeys.git

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
+
 var express = require('express');
+var port = process.env.PORT || 3000;
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.set('port', port);
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use('/', express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
+app.listen(port, function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
